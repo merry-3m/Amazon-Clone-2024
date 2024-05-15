35d311104809f15798c5ea2b3b722ba87c92164d
merrymekoro <merrymekoro3m@gmail.com>
2024-05-14 19:22:08 -0500

    Carousel Effect of Amazon

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header1 from './Components/Header/Header1'
+import CarouselEffect from './Components/Carousel/Carousel'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <Header1/>
+      <CarouselEffect/>
   
     </div>
   )
---
+++ amazon-clone/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import classes from "./carousel.module.css"
+import { Carousel } from "react-responsive-carousel"
+import { img } from './carouselImg' 
+// ` To style the Carousel we import this from npm of Carousel
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const CarouselEffect = () => {
+  return (
+    <div>
+        <Carousel
+        autoplay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+        >
+            {/* import images */}
+            {
+                img.map((imageItemLink) => (
+                    <img src ={imageItemLink}/>
+                ))
+            }
+        </Carousel>
+
+        <div className={classes.hero_img}/>
+
+    </div>
+  )
+}
+
+export default CarouselEffect
